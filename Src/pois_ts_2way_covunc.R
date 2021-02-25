381766a0e8aa57a7e1d1c6c59f5ca09ffdde782e
W. Christohper Carleton <ccarleton@protonmail.com>
2021-02-25 12:07:06 +0100

    saving progress

--- Src/pois_ts_2way_covunc.R
+++ Src/pois_ts_2way_covunc.R
@@ -1,40 +1,64 @@
 poisTSCode <- nimbleCode({
-   B_1 ~ dnorm(mean=0,sd=2)
-   B_2 ~ dnorm(mean=0,sd=1)
-   B_3 ~ dnorm(mean=0,sd=1)
-   mu[1] <- X[1] * (B_1 + (B_2 * (X[1] > a1) + B_3 * (X[1] < a2)))
-   a1 ~ dunif(-100,100)
-   a2 ~ dunif(-100,100)
-   sigma ~ dexp(0.1)
-   lambda0 ~ dnorm(mean=0,sd=2)
-   rho ~ dnorm(0,sd=2)
-   lambda[1] ~ dnorm(rho * lambda0,sd=sigma)
-   for (j in 2:Time){
-      mu[j] <-  X[j] * (B_1 + (B_2 * (X[j] > a1) + B_3 * (X[j] < a2)))
-      lambda[j] ~ dnorm(rho * lambda[j-1],sd=sigma)
-   }
-   for (j in 1:Time){
-      Y[j] ~ dpois(exp(mu[j] + lambda[j]))
-   }
+    ###top-level regression parameters
+    B0 ~ dnorm(mean=0,sd=10)
+    B1 ~ dnorm(mean=0,sd=10)
+    B1ut ~ dnorm(mean=0,sd=5)
+    B1lt ~ dnorm(mean=0,sd=5)
+    sigB0 ~ dunif(1e-10,100)
+    sigB1 ~ dunif(1e-10,100)
+    sigB1ut ~ dunif(1e-10,100)
+    sigB1lt ~ dunif(1e-10,100)
+    ###upper and lower thresholds
+    ut ~ dunif(-100,100)
+    lt ~ dunif(-100,100)
+    ###background autocorrelation
+    sigma ~ dexp(0.1)
+    lambda0 ~ dnorm(mean=0,sd=2)
+    rho ~ dnorm(0,sd=2)
+    lambda[1] ~ dnorm(rho * lambda0,sd=sigma)
+    for (j in 2:Time){
+        lambda[j] ~ dnorm(rho * lambda[j-1],sd=sigma)
+    }
+    ###low-level regression parameters
+    for (k in 1:K){
+        b0[k] ~ dnorm(mean=B0,sd=sigB0)
+        b1[k] ~ dnorm(mean=B1,sd=sigB1)
+        b1ut[k] ~ dnorm(mean=B1ut,sd=sigB1ut)
+        b1lt[k] ~ dnorm(mean=B1lt,sd=sigB1lt)
+        for (j in 1:Time){
+            mu[j,k] <-  b0[k] + X[j,k] * (b1[k] + (b1ut[k] * (X[j,k] > ut) + b1lt[k] * (X[j,k] < lt)))
+            Y[j,k] ~ dpois(exp(mu[j,k] + lambda[j]))
+        }
+    }
 })
 
-##
-K <- 1#ncol(X)
+## Data
+K <- ncol(Xs)
+Y <- matrix(rep(Y,K),ncol=K)
 
-poisTSData <- list(Y=Y,
-                  X=X)
+poisTSData <- list(Y=Y[3:196,],
+                  X=Xs[3:196,])
 
-poisTSConsts <- list(Time=Time,
+poisTSConsts <- list(Time=length(Y[3:196]),
                      K=K)
 
 poisTSInits <- list(lambda0=0,
                      sigma=1,
                      rho=0,
-                     a1=1,
-                     a2=1,
-                     B_1=rep(0,K),
-                     B_2=rep(0,K),
-                     B_3=rep(0,K))
+                     ut=1,
+                     lt=1,
+                     B0=0,
+                     B1=0,
+                     B1ut=0,
+                     B1lt=0,
+                     sigB0=0.1,
+                     sigB1=0.1,
+                     sigB1ut=0.1,
+                     sigB1lt=0.1,
+                     b0=rep(0,K),
+                     b1=rep(0,K),
+                     b1ut=rep(0,K),
+                     b1lt=rep(0,K))
 
 poisTSModel <- nimbleModel(code=poisTSCode,
                         data=poisTSData,
@@ -46,12 +70,28 @@ C_poisTSModel <- compileNimble(poisTSModel, showCompilerOutput = FALSE)
 
 #configure the MCMC
 poisTSModel_conf <- configureMCMC(poisTSModel,thin=1)
-poisTSModel_conf$removeSampler(c("B_1","B_2","B_3","a1","a2"))
-poisTSModel_conf$addSampler(target = c("B_1", "B_2", "B_3"), type = "AF_slice")
-poisTSModel_conf$addSampler(target = c("a1","a2"), type = "RW_block")
+poisTSModel_conf$removeSampler(c(
+                                "B0",
+                                "B1",
+                                "B1ut",
+                                "B1lt",
+                                "ut",
+                                "lt",
+                                "b0",
+                                "b1",
+                                "b1ut",
+                                "b1lt"))
+poisTSModel_conf$addSampler(target = c("B0", "B1", "B1ut","B1lt"), type = "AF_slice")
+
+poisTSModel_conf$addSampler(target = c("ut","lt"), type = "RW_block")
+
+for(k in 1:K){
+   poisTSModel_conf$addSampler(target=c(paste("b0[",k,"]",sep=""),paste("b1[",k,"]",sep=""),paste("b1ut[",k,"]",sep=""),paste("b1lt[",k,"]",sep="")),type="AF_slice")
+}
 
 #select the variables that we want to monitor in the MCMC chain
-#poisTSModel_conf$addMonitors(c("mu"))
+poisTSModel_conf$monitors <- c("B0","B1","B1ut","B1lt","ut","lt","sigB0","sigB1","sigB1ut","sigB1lt")
+poisTSModel_conf$addMonitors2(c("b0","b1","b1ut","b1lt","rho","sigma"))
 
 #build MCMC
 poisTSModelMCMC <- buildMCMC(poisTSModel_conf,enableWAIC=F)
--- Src/simdata_2way_covunc.R
+++ Src/simdata_2way_covunc.R
@@ -1,30 +1,34 @@
 Time <- 200
 X <- rnorm(mean=0,n=Time,sd=1)
-Ts <- do.call(rbind,lapply(1:200,function(x)rnorm(n=50,mean=x,sd=1)))
-apply(Ts,2,function(j)approx(x=j,y=X,xout=1:200))
+ChronoUnc <- 0.5
+K <- 5
+Ts <- do.call(rbind,lapply(1:Time,function(x)rnorm(n=K,mean=x,sd=ChronoUnc)))
+apply(Ts,2,function(j)approx(x=j,y=X,xout=1:Time))
 
 Xs <- apply(Ts,2,function(j){
-        sample_X=approx(x=j,y=X,xout=1:200)
+        sample_X=approx(x=j,y=X,xout=1:Time)
         return(sample_X$y)})
 
 A1 <- 1.5
 A2 <- -1.5
+B_0 <- 1
 B_1 <- 0
-B_2 <- 0
+B_2 <- 1.5
 B_3 <- 0
 
-mu <- c()
-mu[1] <- X[1] * (B_1 + (B_2 * (X[1] > A1) + B_3 * (X[1] < A2)))
-
+#background autocorrelation
 l <- c()
-l0 <- 1
+l0 <- 0.5
 rho = 0.9
-sig <- 0.4
+sig <- 0.2
 l[1] <- rnorm(n=1, mean = rho * l0, sd=sig)
-
 for(j in 2:Time){
-   l[j] <- rnorm(n=1, mean = rho * l[j-1],sd=sig)
-   mu[j] <- X[j] * (B_1 + (B_2 * (X[j] > A1) + B_3 * (X[j] < A2)))
+    l[j] <- rnorm(n=1, mean = rho * l[j-1],sd=sig)
+}
+
+mu <- c()
+for(j in 1:Time){
+   mu[j] <- B_0 + X[j] * (B_1 + (B_2 * (X[j] > A1) + B_3 * (X[j] < A2)))
 }
 
 Y <- rpois(n=Time,lambda=exp(mu + l))
